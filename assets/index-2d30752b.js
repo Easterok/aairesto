d34ff891b202488a3733dad204eb568c892eca7e
Easterok <beskrovnyk@gmail.com>
2025-03-20 22:00:54 +0300

    batman 2.0

---
+++ assets/index-2d30752b.js
@@ -0,0 +1,1 @@
+import{o as e,c as o,a as t}from"./index-ed930d81.js";const r={xmlns:"http://www.w3.org/2000/svg",fill:"none",stroke:"currentColor","stroke-linecap":"round","stroke-linejoin":"round","stroke-width":"2",viewBox:"0 0 24 24"},n=t("path",{d:"M5 12h14M12 5l7 7-7 7"},null,-1),s=[n];function c(a,l){return e(),o("svg",r,[...s])}const d={render:c};export{d as default,c as render};
--- assets/index-c5b217e8.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as o,a as t}from"./index-5702d411.js";const n={xmlns:"http://www.w3.org/2000/svg",fill:"none",stroke:"currentColor","stroke-linecap":"round","stroke-linejoin":"round","stroke-width":"2",viewBox:"0 0 24 24"},r=t("path",{d:"M5 12h14"},null,-1),s=[r];function c(a,i){return e(),o("svg",n,[...s])}const d={render:c};export{d as default,c as render};
