49fd922946cd50bcc484db6ac3fe9af3b4ae5513
Easterok <beskrovnyk@gmail.com>
2025-04-11 23:39:35 +0300

    feat: release

---
+++ assets/index-dee5c87c.js
@@ -0,0 +1,1 @@
+const a=new Array(6).fill("").map((r,e)=>({id:`-${e}`,number:`${e}`,zone:"Зал 2 этаж",active:!0,capacity:2,is_reservable:!0}));export{a as m};
---
+++ assets/index-f9111cae.js
@@ -0,0 +1,1 @@
+const i=t=>{t.setAttribute("data-ark-data-list-item",""),t.setAttribute("role","option"),t.style.justifyContent="initial"},e={mounted:i};export{e as O};
